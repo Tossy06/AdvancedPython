34030cf6be6c52ca1f80abd449f0c7cd92836d91
Tossy06 <davidgomez.sp06@gmail.com>
2025-01-16 14:27:41 -0500

    Second class finished

--- PythonParaWeb/Django/myapp/views.py
+++ PythonParaWeb/Django/myapp/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+
+def index(request):
+    title = 'Welcome  To Django Course'
+    return render(request,'index.html', {'title': title})
+
+def hello(request, username):
+    print(username)
+    return HttpResponse("<h1>Hello %s</h1>" % username)
 
 
 def about(request):
-    return HttpResponse("<h1>About....</h1>")
+    return render(request, "about.html")
+
+def projects(request):
+    #projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {'projects': projects})
+
+
+def tasks(request):
+    #task = get_object_or_404(Task, id = id)
+
+    tastks = Task.objects.all()
+    return render(request, 'tasks.html', {'tasks': tastks})
